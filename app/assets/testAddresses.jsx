6fc5cd3433b2f0d164dd5280e6c779437c57aa44
Ryan Arment <arment.ryan@gmail.com>
2024-07-18 07:41:04 -0600

    products list with valid address by collection

---
+++ app/assets/testAddresses.jsx
@@ -0,0 +1,12 @@
+export const addressValid = [
+'123 Main St, MtnTown, Colorado, 12345',
+'231 Elm St, Cityville, Missouri, 12345',
+'321 Oak St, Ranchberg, Texas, 12345',
+'123 Maple St, Hometown, Colorado, 12345',
+'231 Pine St, Hometown, Missouri, 12345',
+'321 Cedar St, Hometown, Texas, 12345',
+'123 Birch St, Rockville, Colorado, 12345',
+'231 Spruce St, Lakespur, Missouri, 12345',
+'321 Redwood St, Cowland, Texas, 12345',
+'123 Ash St, Beachston, Hawaii, 12345',
+]
--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,7 +1,17 @@
 import {defer} from '@shopify/remix-oxygen';
-import {Await, useLoaderData, Link, Form, useActionData} from '@remix-run/react';
+import {
+  Await,
+  useLoaderData,
+  Link,
+  Form,
+  useActionData,
+  useFetcher
+} from '@remix-run/react';
 import {Suspense, useState} from 'react';
-import {Image, Money} from '@shopify/hydrogen';
+import {CartForm, Image, Money} from '@shopify/hydrogen';
+import {addressValid} from '../assets/testAddresses';
+import {json} from '@shopify/remix-oxygen';
+
 
 /**
  * @type {MetaFunction}
@@ -14,92 +24,124 @@ export const meta = () => {
  * @param {LoaderFunctionArgs} args
  */
 export async function loader(args) {
-  // Start fetching non-critical data without blocking time to first byte
-  const deferredData = loadDeferredData(args);
-
-  // Await the critical data required to render initial state of the page
-  const criticalData = await loadCriticalData(args);
-
-  return defer({...deferredData, ...criticalData});
+  return defer({});
 }
 
-import { json } from '@shopify/remix-oxygen';
-
-export const action = async ({ request }) => {
+export const action = async ({request, context}) => {
   const formData = await request.formData();
-  const address = formData.get('address');
+  const inputAddress = formData.get('address');
+  console.log('input address :', inputAddress);
 
-  return json({ address });
+  // Find the full address that includes the input address
+  const addressExists = addressValid.find(
+    (addr) => addr.toLowerCase() === inputAddress.toLowerCase(),
+  );
+  console.log('address exists : ', addressExists);
+
+  if (addressExists) {
+    // Extract the state name from the full address
+    const stateName = addressExists.split(', ')[2];
+    console.log('Extracted state name:', stateName); // Log the state name
+
+    try {
+      //fetch collection by state name
+      const collectionResponse = await context.storefront.query(
+        FEATURED_COLLECTION_QUERY,
+        {variables: {title: stateName}},
+      );
+      console.log(
+        'collectionResponse response ',
+        JSON.stringify(collectionResponse, null, 2),
+      );
+
+    
+      return json({
+        collection: collectionResponse,
+        addressValid: true,
+      });
+    } catch (error) {
+      console.error('ERROR FETCHING DATA', error);
+      return json({addressValid: false});
+    }
+  } else {
+    return json({addressValid: false});
+  }
 };
 
-/**
- * Load data necessary for rendering content above the fold. This is the critical data
- * needed to render the page. If it's unavailable, the whole page should 400 or 500 error.
- * @param {LoaderFunctionArgs}
- */
-async function loadCriticalData({context}) {
-  const [{collections}] = await Promise.all([
-    context.storefront.query(FEATURED_COLLECTION_QUERY),
-    // Add other queries here, so that they are loaded in parallel
-  ]);
 
-  return {
-    featuredCollection: collections.nodes[0],
-  };
-}
-
-/**
- * Load data for rendering content below the fold. This data is deferred and will be
- * fetched after the initial page load. If it's unavailable, the page should still 200.
- * Make sure to not throw any errors here, as it will cause the page to 500.
- * @param {LoaderFunctionArgs}
- */
-function loadDeferredData({context}) {
-  const recommendedProducts = context.storefront
-    .query(RECOMMENDED_PRODUCTS_QUERY)
-    .catch((error) => {
-      // Log query errors, but don't throw them so the page can still render
-      console.error(error);
-      return null;
-    });
-
-  return {
-    recommendedProducts,
-  };
-}
 
 export default function Homepage() {
-  /** @type {LoaderReturnData} */
-  const data = useLoaderData();
-  const actionData = useActionData();
-  const [address, setAddress] = useState(actionData?.address || '');
+  const actionData = useActionData(); //Get data from action
+  const [address, setAddress] = useState(''); //State for address input
+  const fetcher = useFetcher();// fetcher used for form submission
 
+  // address input change
   const handleInput = (event) => {
     setAddress(event.target.value);
-  };
+  }
 
+  //add to cart and redirect to /cart page
+  const handleAddToCart = (productId) => {
+    fetcher.submit(
+      {
+        action: CartForm.ACTIONS.LinesAdd, // add lines to cart
+        'lines[0]': '1', // limit to one quantity of item
+        'lines[0][merchandiseId]': productId, 
+        redirectTo: '/cart',
+      },
+      { method: 'post', action: '/cart' }
+    );
+  }
+  
+  console.log('Action Data:', actionData); 
+  
   return (
-    <div className="home">
+    <div className="homeSearch">
       <Form method="POST">
-        <label htmlFor='address'>ENTER ADDRESS</label>
-        <input 
-          type='text'
-          id='address'
-          name='address'
+        <label htmlFor="address">ENTER ADDRESS</label>
+        <input
+          type="text"
+          id="address"
+          name="address"
           value={address}
           onChange={handleInput}
-          />
-          <button type='submit'>show offers</button>
+        />
+        <button type="submit">Show Offers</button>
       </Form>
 
-      {actionData &&(
+      {actionData && (
         <div>
-          <h2>{actionData.address}</h2>
+          {actionData.addressValid ? (
+            actionData.collection ? (
+              <div>
+                <h2>{actionData.collection.collections.nodes[0].title}</h2>
+                <div style={{ display: 'flex', overflowX: 'auto' }}>
+                  {actionData.collection.collections.nodes[0].products.edges.map(({ node }) => (
+                    <div 
+                      key={node.id} 
+                      style={{ flex: '0 0 auto', marginRight: '20px' }}
+                      onClick={()=> handleAddToCart(node.id)}
+                    >
+                      {
+                      <img 
+                        src= {node.images.edges[0].node.url} 
+                        alt={node.images.edges[0].node.altText}
+                        style={{ width:'300px', height:'auto'}}/>
+                      }
+                      <h3>{node.title}</h3>
+                      {/* <p>{node.priceRange.minVariantPrice.amount} {node.priceRange.minVariantPrice.currencyCode}</p> */}
+                    </div>
+                  ))}
+                  </div>
+              </div>
+            ) : (
+              <p>Loading collection...</p>
+            )
+          ) : (
+            <p>Sorry, no fiber for you</p>
+          )}
         </div>
       )}
-
-      <FeaturedCollection collection={data.featuredCollection} />
-      <RecommendedProducts products={data.recommendedProducts} />
     </div>
   );
 }
@@ -110,7 +152,9 @@ export default function Homepage() {
  * }}
  */
 
-<Form>Address Search</Form>
+<Form>Address Search</Form>;
+
+
 
 /**
  * @param {{
@@ -135,47 +179,6 @@ function FeaturedCollection({collection}) {
   );
 }
 
-/**
- * @param {{
- *   products: Promise<RecommendedProductsQuery | null>;
- * }}
- */
-function RecommendedProducts({products}) {
-  return (
-    <div className="recommended-products">
-      <h2>Catalog</h2>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Await resolve={products}>
-          {(response) => (
-            <div className="recommended-products-grid">
-              {response
-                ? response.products.nodes.map((product) => (
-                    <Link
-                      key={product.id}
-                      className="recommended-product"
-                      to={`/products/${product.handle}`}
-                    >
-                      {/* <Image
-                        data={product.images.nodes[0]}
-                        aspectRatio="1/1"
-                        sizes="(min-width: 45em) 20vw, 50vw"
-                      /> */}
-                      <h4>{product.title}</h4>
-                      <small>
-                        <Money data={product.priceRange.minVariantPrice} />
-                      </small>
-                      <small>{product.id}</small>
-                    </Link>
-                  ))
-                : null}
-            </div>
-          )}
-        </Await>
-      </Suspense>
-      <br />
-    </div>
-  );
-}
 
 const FEATURED_COLLECTION_QUERY = `#graphql
   fragment FeaturedCollection on Collection {
@@ -189,10 +192,27 @@ const FEATURED_COLLECTION_QUERY = `#graphql
       height
     }
     handle
+    products(first:5) {
+      edges {
+        node {
+          id
+        	title
+					images(first:1){
+            edges{
+              node{
+                url
+                altText
+                id
+              }
+            }
+          }
+        }
+      }
+    }
   }
-  query FeaturedCollection($country: CountryCode, $language: LanguageCode)
-    @inContext(country: $country, language: $language) {
-    collections(first: 1, sortKey: UPDATED_AT, reverse: true) {
+
+  query FeaturedCollection($title: String!) {
+    collections(first: 1, query: $title) {
       nodes {
         ...FeaturedCollection
       }
@@ -200,39 +220,200 @@ const FEATURED_COLLECTION_QUERY = `#graphql
   }
 `;
 
-const RECOMMENDED_PRODUCTS_QUERY = `#graphql
-  fragment RecommendedProduct on Product {
-    id
-    title
-    handle
-    priceRange {
-      minVariantPrice {
-        amount
-        currencyCode
-      }
-    }
-    images(first: 1) {
-      nodes {
-        id
-        url
-        altText
-        width
-        height
-      }
-    }
-  }
-  query RecommendedProducts ($country: CountryCode, $language: LanguageCode)
-    @inContext(country: $country, language: $language) {
-    products(first: 4, sortKey: UPDATED_AT, reverse: true) {
-      nodes {
-        ...RecommendedProduct
-      }
-    }
-  }
-`;
+
 
 /** @typedef {import('@shopify/remix-oxygen').LoaderFunctionArgs} LoaderFunctionArgs */
 /** @template T @typedef {import('@remix-run/react').MetaFunction<T>} MetaFunction */
 /** @typedef {import('storefrontapi.generated').FeaturedCollectionFragment} FeaturedCollectionFragment */
 /** @typedef {import('storefrontapi.generated').RecommendedProductsQuery} RecommendedProductsQuery */
 /** @typedef {import('@shopify/remix-oxygen').SerializeFrom<typeof loader>} LoaderReturnData */
+
+
+// G R A V E Y A R D 
+
+
+// /**
+//  * Load data necessary for rendering content above the fold. This is the critical data
+//  * needed to render the page. If it's unavailable, the whole page should 400 or 500 error.
+// //  * @param {LoaderFunctionArgs}
+// //  */
+// // async function loadCriticalData({context}) {
+// //   const [{collections}] = await Promise.all([
+// //     //context.storefront.query(FEATURED_COLLECTION_QUERY)
+// //     //,
+// //     ///
+// //     //context.storefront.query(LOAD_ALL_COLLECTIONS_QUERY),
+// //     ///
+// //     // Add other queries here, so that they are loaded in parallel
+// //   ]);
+
+// //   return {
+// //    // featuredCollection: collections.nodes[0],
+// //    };
+// // }
+
+// /**
+//  * Load data for rendering content below the fold. This data is deferred and will be
+//  * fetched after the initial page load. If it's unavailable, the page should still 200.
+//  * Make sure to not throw any errors here, as it will cause the page to 500.
+//  * @param {LoaderFunctionArgs}
+//  */
+// function loadDeferredData({context}) {
+//   const recommendedProducts = context.storefront
+//     .query(RECOMMENDED_PRODUCTS_QUERY)
+//     .catch((error) => {
+//       // Log query errors, but don't throw them so the page can still render
+//       console.error(error);
+//       return null;
+//     });
+
+//   return {
+//     recommendedProducts,
+//   };
+// }
+
+// const RECOMMENDED_PRODUCTS_QUERY = `#graphql
+//   fragment RecommendedProduct on Product {
+//     id
+//     title
+//     handle
+//     priceRange {
+//       minVariantPrice {
+//         amount
+//         currencyCode
+//       }
+//     }
+//     images(first: 1) {
+//       nodes {
+//         id
+//         url
+//         altText
+//         width
+//         height
+//       }
+//     }
+//   }
+//   query RecommendedProducts ($country: CountryCode, $language: LanguageCode)
+//     @inContext(country: $country, language: $language) {
+//     products(first: 4, sortKey: UPDATED_AT, reverse: true) {
+//       nodes {
+//         ...RecommendedProduct
+//       }
+//     }
+//   }
+// `;
+
+// /**
+//  * @param {{
+//  *   products: Promise<RecommendedProductsQuery | null>;
+//  * }}
+//  */
+// function RecommendedProducts({products}) {
+//   return (
+//     <div className="recommended-products">
+//       <h2>Available Offers</h2>
+//       <Suspense fallback={<div>Loading...</div>}>
+//         <Await resolve={products}>
+//           {(response) => (
+//             <div className="recommended-products-grid">
+//               {response
+//                 ? response.products.nodes.map((product) => (
+//                     <Link
+//                       key={product.id}
+//                       className="recommended-product"
+//                       to={`/products/${product.handle}`}
+//                     >
+//                       {/* <Image
+//                         data={product.images.nodes[0]}
+//                         aspectRatio="1/1"
+//                         sizes="(min-width: 45em) 20vw, 50vw"
+//                       /> */}
+//                       <h4>{product.title}</h4>
+//                       <small>
+//                         <Money data={product.priceRange.minVariantPrice} />
+//                       </small>
+//                       <small>{product.id}</small>
+//                     </Link>
+//                   ))
+//                 : null}
+//             </div>
+//           )}
+//         </Await>
+//       </Suspense>
+//       <br />
+//     </div>
+//   );
+// }
+
+
+// async function fetchAllCollections(context) {
+//   let allCollections = [];
+//   let hasNextPage = true;
+//   let cursor = null;
+
+//   while (hasNextPage) {
+//     try {
+//       const response = await context.storefront.query(LOAD_ALL_COLLECTIONS_QUERY, {
+//         variables: { cursor },
+//       });
+//      console.log('Query response:', JSON.stringify(response, null, 2));  // Log the response
+//       if (response != null) {
+//         console.log('in fetchAllCollection if()')
+//         const { edges, pageInfo } = response.collections;
+//         allCollections = allCollections.concat(edges.map(edge => edge.node));
+//         hasNextPage = pageInfo.hasNextPage;
+//         cursor = pageInfo.endCursor;
+//       } else {
+//         console.error('No collections found in the response');
+//         hasNextPage = false;  // Stop the loop if no collections found
+//       }
+//     } catch (error) {
+//       console.error('****Error fetching collections:', error);
+//       hasNextPage = false;  // Stop the loop on error
+//     }
+//   }
+
+//   return allCollections;
+// }
+// const LOAD_ALL_COLLECTIONS_QUERY = `#graphql
+//   query LoadAllCollections($cursor: String) {
+//     collections(first: 5, after: $cursor) {
+//       pageInfo {
+//         hasNextPage
+//         endCursor
+//       }
+//       edges {
+//         node {
+//           id
+//           title
+//           handle
+//           updatedAt
+//           products(first: 5) {
+//             edges {
+//               node {
+//                 id
+//                 title
+//                 handle
+//                 priceRange {
+//                   minVariantPrice {
+//                     amount
+//                     currencyCode
+//                   }
+//                 }
+//                 images(first: 1) {
+//                   edges {
+//                     node {
+//                       src
+//                       altText
+//                     }
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
+
